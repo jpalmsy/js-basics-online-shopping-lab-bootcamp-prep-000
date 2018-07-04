bceba72d55be60d26ae462976d650cc267395197
Joey Palmieri <joeypalmieri@hotmail.com>
2018-07-04 00:19:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function generatecartdescription() {
     for (var i=1; i<getCart().length - 1; i++ ) {
       middleCartDescription += ', $(getCart()[i].itemName} at $${getCart()[i].itemPrice}'
     }
-    cartDescription += '${middleCartDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().legnth'
+    cartDescription += '${middleCartDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1.itemPrice'
     }
   }
 }
