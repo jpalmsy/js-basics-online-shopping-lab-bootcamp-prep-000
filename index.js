19f521b8718c97635c36d43c65100196b8642f25
Joey Palmieri <joeypalmieri@hotmail.com>
2018-07-04 00:08:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,21 +10,32 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemname = cartitem(item)
+ getCart().push(item)
+ return '{$item.itemName} has been added to your cart.'
 }
 
 function viewCart() {
-  // write your code here
+  return getcart.length === 0 ? "Your shopping cart is empty": generatecartdescription()
 }
 
 function total() {
-  // write your code here
+  var sum = sumPrices ()
+  return sum
 }
 
-function removeFromCart(item) {
-  // write your code here
+function removeFromCart(itemName) {
+  var itemToRemove = searcgCartForItemToRemove(itemName)
+  return itemtoRemove ? removeItemFromCart(itemToRemove): notifyUserThereIsNoItemToRemove()
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
-}
+  if (arguments[0] === undefined) {
+    return "Sorry, we dont have a credit card on file for you." 
+  } else {
+    var sumToCharge = total()
+    setCart ([])
+    return 'Your total cost is $${sumToCharge}, which will be charged to the card ${cardNumber}.'
+  }
+  }
+
