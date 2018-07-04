14ec98c7d4a58780052c25a9aac89fc8835583fe
Joey Palmieri <joeypalmieri@hotmail.com>
2018-07-04 00:32:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var item = cartitem(item)
+ var item = generateCartitem(item)
  getCart().push(item)
  return '{$item.itemName} has been added to your cart.'
 }
